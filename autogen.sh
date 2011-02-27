2ed06234256be8fa0bb85785491fe6134b67a5b3
Peter de Ridder <peter@xfce.org>
2011-02-27 18:42:13 +0100

    Changed the copyright statements to GPL 2

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,35 @@
 #!/bin/sh
 #
-# Copyright (c) 2002-2006
-#         The Thunar development team. All rights reserved.
+#-
+# Copyright (C) 2007-2011  Peter de Ridder <peter@xfce.org>
 #
-# Written for Thunar by Benedikt Meurer <benny@xfce.org>.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+XDT_REQURED_VERSION="4.8.0"
 
 (type xdt-autogen) >/dev/null 2>&1 || {
   cat >&2 <<EOF
-autogen.sh: You don't seem to have the Xfce development tools installed on
-            your system, which are required to build this software.
-            Please install the xfce4-dev-tools package first, it is available
+autogen.sh: You don't seem to have the Xfce development tools (at least
+            version $XDT_REQURED_VERSION) installed on your system, which
+            are required to build this software.
+            Please install the xfce4-dev-tools package first; it is available
             from http://www.xfce.org/.
 EOF
   exit 1
 }
 
-XDT_AUTOGEN_REQUIRED_VERSION="4.7.0" exec xdt-autogen $@
+XDT_AUTOGEN_REQUIRED_VERSION=$XDT_REQURED_VERSION exec xdt-autogen "$@"
 
 # vi:set ts=2 sw=2 et ai:
