dcc669e5f54fc71b7d6c34e57a604229bc38408c
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2024-04-03 19:07:12 +0700

    Fix autotools warning, get rid of intltool

--- autogen.sh
+++ autogen.sh
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-XDT_REQURED_VERSION="4.8.0"
+XDT_REQURED_VERSION="4.17.0"
 
 (type xdt-autogen) >/dev/null 2>&1 || {
   cat >&2 <<EOF
