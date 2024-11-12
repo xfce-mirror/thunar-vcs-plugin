72812186b77c21728c1e4d60c5cb779aae99582a
correctmost <11866-correctmost@users.noreply.gitlab.xfce.org>
2024-11-12 16:58:49 -0500

    Fix GOptionContext leak in tvp-git-helper main

--- tvp-git-helper/main.c
+++ tvp-git-helper/main.c
@@ -196,6 +196,8 @@ int main (int argc, char *argv[])
     g_error_free(error);
   }
 
+  g_option_context_free(option_context);
+
   if(print_version)
   {
     g_print(PACKAGE_STRING "\n\tcompiled on " __DATE__ ", " __TIME__ "\n");
